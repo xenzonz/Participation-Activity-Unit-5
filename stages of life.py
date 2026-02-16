d8ddce3a97c0dfdbe7eae0de0c35861898f4df97
sam cocquyt <samcocquyt04@gmail.com>
2026-02-15 22:24:28 -0500

    improved logic, further improved print statements, docstring adde

--- stages of life.py
+++ stages of life.py
@@ -1,16 +1,23 @@
-print("hello world")
+"""
+Docstring for Participation Activity Unit 5
+i. Stages of Life 5-6 in the book | Participation Activity Unit 5
+ii. Sam Cocquyt
+iii. if-elif-else chain about a persons stage of life
+iv. No starter code
+v. 2/15/2026
+"""
 
 age = 90
 
 if age < 2:
-    print("baby")
+    print("This person is a baby")
 elif age < 4:
-    print("toddler")
+    print("This person is a toddler")
 elif age < 13:
-    print("kid")
+    print("This person is a kid")
 elif age < 20:
-    print("teenager")
+    print("This person is a teenager")
 elif age < 65:
-    print("adult")
-elif age > 65:
-    print("elder")
+    print("This person is an adult")
+else:
+    print("This person is an elder")
